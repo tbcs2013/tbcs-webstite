9db7619c15b5b390b36289b5fcbd55be69491a54
enochmalamba <enochmalamba@gmail.com>
2025-06-09 13:22:09 +0200

    made a lot of changes

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+const header = document.querySelector("header");
+
+//check if window is already scrolled after refresh
+if (window.scrollY > 0) {
+  header.classList.add("scrolled");
+}
+//change header background on scroll
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
 // Add particle effect to hero section
 function createParticles() {
   const hero = document.querySelector(".hero");
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 70; i++) {
     const particle = document.createElement("div");
     particle.style.position = "absolute";
-    particle.style.width = "2px";
-    particle.style.height = "2px";
-    particle.style.backgroundColor = "rgba(64, 123, 255, 0.5)";
+    particle.style.width = "4px";
+    particle.style.height = "4px";
+    particle.style.backgroundColor = "white";
     particle.style.borderRadius = "50%";
     particle.style.left = Math.random() * 100 + "%";
     particle.style.top = Math.random() * 100 + "%";
